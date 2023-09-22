a67a49f08da8b6ebf995c31cfb88614798dc8cf2
itsChrizZ <christoph.sm@e.mail.de>
2023-09-22 09:17:57 +0200

    created class Fleet and empty methods

---
+++ src/Fleet.java
@@ -0,0 +1,44 @@
+import java.util.Map;
+import java.util.Set;
+
+public class Fleet {
+
+	private Map<Ship, Integer> ships;
+	
+	
+	public Fleet() {
+		
+	}
+	
+	public boolean addShip(String shipname, int capacity) {	
+		return true;
+	}
+	
+	public boolean addShip(String shipname, int capacity, int shipcount) {		
+		return true;
+	}
+	
+	public boolean removeShip(String shipname) {
+		return true;
+	}
+	
+	public boolean removeShip(String shipname, int shipcount) {
+		return true;
+	}
+	
+	public Set<Ship> getShips() {
+		return ships.keySet();
+	}
+	
+	public int getShipAmount(Ship ship) {
+		return ships.get(ship);
+	}
+	
+	public boolean load(String path) {
+		return true;
+	}
+	
+	public boolean save(String path) {
+		return true;
+	}
+}
