677cbf52218b578c2db90a64f2c7f7e8c8baeb5e
sloorx <sloorx@mailbox.org>
2023-10-07 18:40:20 +0200

    Implemented Quest, fixed typos

--- src/Quest.java
+++ src/Quest.java
@@ -1,29 +1,42 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class Quest {
-    private Map<String, Integer> ressources;
+    private Map<String, Integer> resources;
 
     public Quest(){
+        resources = new HashMap<String, Integer>();
     }
 
-    public boolean addRessource(String ressourse, Integer amount){
+    public boolean addResource(String resource, Integer amount){
+        if(resources.containsKey(resource)){
+            return false;
+        }
+        resources.put(resource, amount);
         return true;
     }
 
-    public boolean editRessource(String ressourse, Integer amount){
-        return true;
+    public boolean editResource(String resource, Integer amount){
+        if(resources.containsKey(resource)){
+            resources.put(resource, amount);
+            return true;
+        }
+        return false;
     }
 
-    public boolean removeRessource(String ressourse){
-        return true;
+    public boolean removeResource(String resource){
+        if(resources.remove(resource) != null){
+            return true;
+        }
+        return false;
     }
 
-    public int getValue(String ressource){
-        return 0;
+    public int getValue(String resource){
+        return resources.get(resource);
     }
 
-    public Set<String> getRessources(){
-        return null;
+    public Set<String> getResources(){
+        return resources.keySet();
     }
 }
