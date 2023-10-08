927565b90f903628eab653c34e580c4e6ab7414d
sloorx <sloorx@mailbox.org>
2023-10-08 20:38:02 +0200

    Added basic structure

--- src/ToolController.java
+++ src/ToolController.java
@@ -3,13 +3,46 @@ import java.util.concurrent.LinkedBlockingDeque;
 public class ToolController {
 
     private LinkedBlockingDeque<GUIEvent> eventQueue;
+    private Fleet fleet;
+    private Quest quest;
 
     public ToolController(){
         eventQueue = new LinkedBlockingDeque<GUIEvent>();
+        fleet = Fleet.getInstance();
+        quest = new Quest();
     }
 
     public void run(){
-
+        GUIEvent event;
+        while(true){
+            if(!eventQueue.isEmpty()){
+                event = eventQueue.poll();
+                switch (event.getType()) {
+                    case SHIP_ADDED:
+                        break;
+                    case SHIP_EDITED:
+                        break;
+                    case SHIP_REMOVED:
+                        break;
+                    case RESOURCE_ADDED:
+                        break;
+                    case RESOURCE_EDITED:
+                        break;
+                    case RESOURCE_REMOVED:
+                        break;
+                    case SOLVE:
+                        break;
+                    case SAVE:
+                        break;
+                    case LOAD:
+                        break;
+                    case EXIT:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
 
     public void update(GUIEvent e){
