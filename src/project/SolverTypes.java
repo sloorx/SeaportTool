eb587e1d47cb795aaae04b8e9e978e5082ad214f
sloorx <sloorx@mailbox.org>
2023-10-13 09:07:07 +0200

    Added parts of eventhandling

---
+++ src/project/SolverTypes.java
@@ -0,0 +1,9 @@
+package project;
+public enum SolverTypes {
+    CAPACITY_FH,
+    TIME_FH,
+    CAPACITY_CM,
+    TIME_CM,
+    CAPACITY_SL,
+    TIME_SL
+}
--- src/project/ToolController.java
+++ src/project/ToolController.java
@@ -1,5 +1,6 @@
 package project;
 
+import java.util.List;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class ToolController {
@@ -7,6 +8,7 @@ public class ToolController {
     private LinkedBlockingDeque<GUIEvent> eventQueue;
     private Fleet fleet;
     private Quest quest;
+    private Thread thread;
 
     public ToolController(){
         eventQueue = new LinkedBlockingDeque<GUIEvent>();
@@ -16,38 +18,81 @@ public class ToolController {
 
     public void run(){
         GUIEvent event;
+        List<Object> params;
+        thread = Thread.currentThread();
         while(true){
             if(!eventQueue.isEmpty()){
                 event = eventQueue.poll();
+                params = event.getParameters();
                 switch (event.getType()) {
                     case SHIP_ADDED:
+                        fleet.addShip((String) params.get(0), ((Integer) params.get(1)).intValue(), ((Integer) params.get(2)).intValue());
                         break;
                     case SHIP_EDITED:
+                        fleet.editShip((String) params.get(0), (String) params.get(1), ((Integer) params.get(2)).intValue());
                         break;
                     case SHIP_REMOVED:
+                        fleet.removeShip((String) params.get(0), ((Integer) params.get(1)).intValue());
                         break;
                     case RESOURCE_ADDED:
+                        quest.addResource((String) params.get(0), (Integer) params.get(1));
                         break;
                     case RESOURCE_EDITED:
+                        quest.editResource((String) params.get(0), (Integer) params.get(1));
                         break;
                     case RESOURCE_REMOVED:
+                        quest.removeResource((String) params.get(0));
                         break;
                     case SOLVE:
+                        switch ((SolverTypes)params.get(0)) {
+                            case CAPACITY_FH:
+                                //TODO
+                                break;
+                            case TIME_FH:
+                                //TODO
+                                break;
+                            case CAPACITY_CM:
+                                //TODO
+                                break;
+                            case TIME_CM:
+                                //TODO
+                                break;
+                            case CAPACITY_SL:
+                                //TODO
+                                break;
+                            case TIME_SL:
+                                //TODO
+                                break;
+                            default:
+                                break;
+                        }
                         break;
                     case SAVE:
+                        //TODO
                         break;
                     case LOAD:
+                        //TODO
                         break;
                     case EXIT:
+                        System.exit(0);
                         break;
                     default:
                         break;
                 }
+            }else{
+                update(null);
             }
         }
     }
 
-    public void update(GUIEvent e){
-        eventQueue.add(e);
+    public synchronized void update(GUIEvent e){
+        if(Thread.currentThread().equals(this.thread)){
+            try {
+                wait();
+            } catch (InterruptedException e1) {};
+        }else{
+            eventQueue.add(e);
+            notify();
+        }
     }
 }
