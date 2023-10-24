425cc7d11b1c78cc3f65757a04981003d61011ea
itsChrizZ <christoph.sm@e.mail.de>
2023-10-24 22:21:44 +0200

    added creation of ToolGUI und ToolController in main

--- src/project/SeaportTool.java
+++ src/project/SeaportTool.java
@@ -1,8 +1,17 @@
 package project;
 
+import project.GUI.ToolGUI;
+
 public class SeaportTool {
 
     public static void main(String[] args) {
-        //TODO
+    	ToolGUI gui = new ToolGUI();
+    	ToolController controller = new ToolController();
+    	
+    	gui.setController(controller);
+    	controller.setGUI(gui);
+    	
+    	new Thread(gui).start();
+    	controller.run();
     }
 }
