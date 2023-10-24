1c4c8ded3f2bee53765bf24b2ce813920b3c08be
itsChrizZ <christoph.sm@e.mail.de>
2023-10-24 22:24:25 +0200

    added code for run and update methods and closing operation

--- src/project/GUI/ToolGUI.java
+++ src/project/GUI/ToolGUI.java
@@ -1,6 +1,9 @@
 package project.GUI;
 
+import java.util.List;
 import java.util.concurrent.LinkedBlockingDeque;
+
+import project.EventTypes;
 import project.GUIEvent;
 import project.ToolController;
 
@@ -29,25 +32,22 @@ public class ToolGUI extends JFrame implements Runnable {
 	 * Create the frame.
 	 */
 	public ToolGUI() {
-		addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosed(WindowEvent e) {
-				//TODO Exit übergeben
-				//update(null);
-			}
-		});        
+		eventQueue = new LinkedBlockingDeque<GUIEvent>();
+		
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
 		setBounds(100, 100, 493, 401);
 		pnContentPane = new JPanel();
 		pnContentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(pnContentPane);
 		pnContentPane.setLayout(null);
-		
+
 		JPanel pnMain = new JPanel();
 		pnMain.setBounds(0, 45, 477, 400);
 		pnContentPane.add(pnMain);
 		pnMain.setLayout(null);
-		
+
 		fp = new FleetPanel(this);
 		fp.setBounds(0, 0, 477, 400);
 		pnMain.add(fp, BorderLayout.SOUTH);
@@ -62,55 +62,108 @@ public class ToolGUI extends JFrame implements Runnable {
 		questFormGUI.setBounds(0, 0, 477, 400);
 		pnMain.add(questFormGUI, BorderLayout.SOUTH);
 		pnMain.setLayout(new BorderLayout());
-		
+
 		JButton btnShips = new JButton("Flotte");
 		btnShips.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(questPanelGUI.isVisible())
+				if (questPanelGUI.isVisible())
 					questPanelGUI.setVisible(false);
 				fp.setVisible(true);
 			}
 		});
 		btnShips.setBounds(20, 11, 89, 23);
 		pnContentPane.add(btnShips);
-		
+
 		JButton btnQuest = new JButton("Quest");
 		btnQuest.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(fp.isVisible())
+				if (fp.isVisible())
 					fp.setVisible(false);
-				if(questFormGUI.isVisible())
+				if (questFormGUI.isVisible())
 					questFormGUI.setVisible(false);
 				questPanelGUI.setVisible(true);
 			}
 		});
 		btnQuest.setBounds(137, 11, 89, 23);
 		pnContentPane.add(btnQuest);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
 
-		//TODO eigenes Exit-Event, in dem EXIT dem Controller übergeben wird
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setResizable(true);
-        setLocationRelativeTo(null);
-        setVisible(true);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				setVisible(false);
+				updateController(new GUIEvent(EventTypes.EXIT, null));
+			}
+		});
 	}
 
 	@Override
 	public void run() {
+		List<Object> params;		
+		GUIEvent event;
+		
+		while (true) {
+			if (!eventQueue.isEmpty()) {
+				event = eventQueue.poll();
+				params = event.getParameters();
 				
+				switch (event.getType()) {
+				case ERROR:
+					showException((String) params.get(0));
+					break;
+				case SHIP_ADDED:
+					fp.addShipToGUI();
+					break;
+				case SHIP_EDITED:
+
+					break;
+				case SHIP_REMOVED:
+
+					break;
+				case RESOURCE_ADDED:
+
+					break;
+				case RESOURCE_EDITED:
+
+					break;
+				case RESOURCE_REMOVED:
+
+					break;
+				case SOLUTION_ADDED:
+
+					break;
+				default:
+					break;
+				}
+			} 
+//			else {
+//				updateGUI(null);
+//			}
+		}
+			
 	}
-	
+
 	public void setController(ToolController controller) {
 		this.controller = controller;
 	}
-	
-	public void updateController(GUIEvent event) {
-		//TODO
-		//Toolcontroller fuer Update aufrufen
+
+	public synchronized void updateGUI(GUIEvent event) {		
+		if (Thread.currentThread().equals(new Thread(this))) {
+			try {
+				wait();
+			} catch (InterruptedException e1) {
+			}
+			;
+		} else {
+			eventQueue.add(event);
+			notify();
+		}
 	}
-	
-	public void update(GUIEvent event) {
-		//TODO Event an Controller übergeben
-		//controller.update(event);
+
+	public void updateController(GUIEvent event) {
+		controller.update(event);
 	}
 
 	public void openQuestForm() {
@@ -122,13 +175,9 @@ public class ToolGUI extends JFrame implements Runnable {
 		questFormGUI.setVisible(false);
 		questPanelGUI.setVisible(true);
 	}
-	
+
 	public void showException(String msg) {
 		JOptionPane.showMessageDialog(this, msg, "Fehler", JOptionPane.ERROR_MESSAGE);
 	}
-	
-	public static void main(String[] args) {
-		new ToolGUI();
-		
-	}
+
 }
