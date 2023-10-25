c5adc92e6a5f8c8430e6c3fd8444352f62b464e3
sloorx <sloorx@mailbox.org>
2023-10-25 17:02:21 +0200

    Added error messages

--- src/project/ToolController.java
+++ src/project/ToolController.java
@@ -37,7 +37,6 @@ public class ToolController {
 				switch (event.getType()) {
 				case SHIP_ADDED:
 					retval = fleet.addShip((String) params.get(0), ((Integer) params.get(1)).intValue(), ((Integer) params.get(2)));
-					
 					if (retval) {
 						gui.updateGUI(event);
 					} else {
@@ -46,80 +45,81 @@ public class ToolController {
 						gui.updateGUI(new GUIEvent(EventTypes.ERROR, params));
 					}
 					break;
+
 				case SHIP_EDITED:
 					retval = fleet.editShip((String) params.get(0), (String) params.get(1), ((Integer) params.get(2)).intValue());
-					
 					if (retval) {
 						gui.updateGUI(event);
 					} else {
 						params = new ArrayList<>();
-						params.add(""); // TODO
+						params.add("Unbekannter Schiffsname");
 						gui.updateGUI(new GUIEvent(EventTypes.ERROR, params));
 					}
 					break;
+
 				case SHIP_REMOVED:
 					retval = fleet.removeShip((String) params.get(0), ((Integer) params.get(1)).intValue());
-					
 					if (retval) {
 						gui.updateGUI(event);
 					} else {
 						params = new ArrayList<>();
-						params.add(""); // TODO
+						params.add("Unbekannter Schiffsname");
 						gui.updateGUI(new GUIEvent(EventTypes.ERROR, params));
 					}
 					break;
+
 				case RESOURCE_ADDED:
 					retval = quest.addResource((String) params.get(0), (Integer) params.get(1));
-					
 					if (retval) {
 						gui.updateGUI(event);
 					} else {
 						params = new ArrayList<>();
-						params.add(""); // TODO
+						params.add("Ressourcenname bereits in Verwendung");
 						gui.updateGUI(new GUIEvent(EventTypes.ERROR, params));
 					}
 					break;
+
 				case RESOURCE_EDITED:
 					retval = quest.editResource((String) params.get(0), (Integer) params.get(1));
-					
 					if (retval) {
 						gui.updateGUI(event);
 					} else {
 						params = new ArrayList<>();
-						params.add(""); // TODO
+						params.add("Unbekannter Ressourcenname");
 						gui.updateGUI(new GUIEvent(EventTypes.ERROR, params));
 					}
 					break;
+
 				case RESOURCE_REMOVED:
 					retval = quest.removeResource((String) params.get(0));
-					
 					if (retval) {
 						gui.updateGUI(event);
 					} else {
 						params = new ArrayList<>();
-						params.add(""); // TODO
+						params.add("Unbekannter Ressourcenname");
 						gui.updateGUI(new GUIEvent(EventTypes.ERROR, params));
 					}
 					break;
+
 				case SOLVE:
 					switch ((SolverTypes) params.get(0)) {
 					case CAPACITY_FH:
-						// TODO
+						// solver = new CapacitySolverFH();
 						break;
 					case TIME_FH:
-						// TODO
+						// solver = new TimeSolverFH();
 						break;
 					case CAPACITY_CM:
-						// TODO
+						// solver = new CapacitySolverCM();
 						break;
 					case TIME_CM:
-						// TODO
+						// solver = new TimeSolverCM();
 						break;
 					case CAPACITY_SL:
 						solver = new CapacitySolverSL();
 						break;
 					case TIME_SL:
-						// TODO
+						// solver = new TimeSolverSL();
 						break;
 					default:
 						break;
@@ -130,18 +130,23 @@ public class ToolController {
 						gui.updateGUI(new GUIEvent(EventTypes.SOLUTION_ADDED, params));
 					}
 					break;
+
 				case SAVE:
 					// TODO
 					break;
+
 				case LOAD:
 					// TODO
 					break;
+
 				case EXIT:
 					System.exit(0);
 					break;
+
 				default:
 					break;
 				}
+
 			} else {
 				this.update(null); //wait for updates
 			}
